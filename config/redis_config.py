c28996b31467bd6b46908cb5c2d2592420692eb1
CordeiroGusta <guscordeiro199@gmail.com>
2026-09-16 20:23:44 -0300

    feat: Add timeout exception erro in connection module

--- config/redis_config.py
+++ config/redis_config.py
@@ -1,7 +1,11 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class RedisSettings(BaseSettings):
-    model_config = SettingsConfigDict(env_file='.env', env_prefix='REDIS_')
+    model_config = SettingsConfigDict(
+        env_file='.env', 
+        env_prefix='REDIS_',
+        extra='ignore'
+        )
 
     host: str
     port: int
--- database/connection.py
+++ database/connection.py
@@ -1,17 +1,28 @@
 import redis
+from redis.backoff import NoBackoff
+from redis.retry import Retry
 from config.redis_config import settings
 
+no_retry = Retry(NoBackoff(), retries=0)
+
 redis_client = redis.Redis(
-    host = settings.host,
-    port = settings.port,
-    db = settings.db,
-    decode_responses=True
+    host=settings.host,
+    port=settings.port,
+    db=settings.db,
+    decode_responses=True,
+    socket_connect_timeout=2, 
+    socket_timeout=2,         
+    retry=no_retry,
+    retry_on_error=[]
 )
 
 def test_connection():
     try:
         resposta = redis_client.ping()
         return f"Conectado: {resposta}"
+    
+    except (redis.ConnectionError, redis.TimeoutError) as e:
+        return f"Erro de conexão com o servidor Redis (Tempo de Resposta Esgotado/Falha na Conexão): {e}"
 
-    except redis.ConnectionError:
-        return f"Não foi possível se conectar ao banco"
+    except Exception as e:
+        return f"Erro generico: {e}"
